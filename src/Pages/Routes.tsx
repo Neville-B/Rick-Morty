91bf2457ca80400ce18a62cc0f2a9c63ce070ba2
Neville-B <nevillebudiman@gmail.com>
2023-04-20 00:27:11 +0700

    Change routing

--- src/Pages/Routes.tsx
+++ src/Pages/Routes.tsx
@@ -4,11 +4,11 @@ import CharactersList from "./CharactersList";
 
 export const MainRoutes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes >
         <Route path="/rick-morty-app/character-detail" element={<CharacterDetail />} />
         <Route path="/rick-morty-app" element={<CharactersList />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };

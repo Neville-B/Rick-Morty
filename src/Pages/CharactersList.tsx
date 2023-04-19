921855e52c80df159ef91acb0057fe367a6e3e51
Neville-B <nevillebudiman@gmail.com>
2023-04-20 00:40:09 +0700

    Change routing

--- src/Pages/CharactersList.tsx
+++ src/Pages/CharactersList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ApiData } from '../Api/api'
+import { Link } from "react-router-dom";
 
 const CharactersList = () => {
   const [characterList, setCharacterList] = useState([]);
@@ -17,6 +18,7 @@ const CharactersList = () => {
     <>
       <h2>
         <center>Rick and Morty</center>
+        <Link to='character-detail'>Detail</Link>
       </h2>
       {characterList.map((el:any) => {
          return (<li key={el.id}>{el.name}</li>)
